097f6ceb8151bb23e545d83d0629cac12b1f83c9
jklopf <32438813+jklopf@users.noreply.github.com>
2018-12-13 15:41:21 +0100

    deneb job script

---
+++ benchmark/deneb/d1_job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --time=1:0:0
+#SBATCH --qos=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=gpu
+ 
+slmodules -s x86_E5v2_Mellanox_GPU
+module load gcc cuda cudnn mvapich2 openblas
+source ~/tensorflow-1.9/bin/activate
+srun python d1_gpu_test.py
